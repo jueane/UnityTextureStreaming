dc7d2a77e9089584d17d6006ac09c8581469f477
je <je@jueane.com>
2024-01-06 12:52:38 +0800

    texture streaming 编辑器完善

--- Assets/Editor/MipmapAndStreamingWindow.cs
+++ Assets/Editor/MipmapAndStreamingWindow.cs
@@ -1,60 +1,155 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Linq;
 
 public class MipmapAndStreamingWindow : EditorWindow
 {
-    private string folderPath = "Assets/Textures"; // 默认路径
-    private int totalTextureCount;
-    private int mipStreamingEnabledCount;
+    int totalTextureCount;
+    int mipStreamingEnabledCount;
 
-    [MenuItem("Window/Mipmap and Streaming Settings")]
+    List<string> folderPaths = new List<string>(); // 新增一个数组用于存储目录列表
+    int selectedFolderIndex = 0; // 选中的目录索引
+    string selectedFolder => (folderPaths == null || folderPaths.Count < 1) ? null : folderPaths[selectedFolderIndex];
+
+    Vector2 scrollPosition = Vector2.zero;
+
+    [MenuItem("Window/Texture Streaming Settings")]
     static void OpenWindow()
     {
         MipmapAndStreamingWindow window = GetWindow<MipmapAndStreamingWindow>();
         window.Show();
     }
 
+    int count;
+
+    string filePath = "Assets/Editor/TextureStreamingList.txt";
+
     void OnGUI()
     {
         GUILayout.Label("Mipmap and Streaming Settings", EditorStyles.boldLabel);
 
-        // 将当前所选中的Project窗口的目录赋值给Folder path按钮
-        if (GUILayout.Button("Use Selected Folder"))
+        // 添加文本框
+        GUILayout.Label($"File Path: {filePath}");
+
+        if (folderPaths == null || count != folderPaths.Count)
         {
-            UseSelectedFolder();
+            count = folderPaths?.Count ?? 0;
+            Debug.Log($"当前数量：{count}");
+            count = folderPaths.Count;
         }
 
-        // 显示当前路径
-        GUILayout.Label("Current Folder Path: " + folderPath);
+        GUILayout.BeginHorizontal();
 
-        // 提供文本输入框来修改路径
-        folderPath = EditorGUILayout.TextField("Folder Path", folderPath);
-
-        // 应用设置按钮
-        if (GUILayout.Button("Apply Mipmaps and Streaming"))
+        // 新增按钮，用于增加目录
+        if (GUILayout.Button("Add Folder"))
         {
-            ApplyMipmapsAndStreaming();
+            // 使用 "Use Selected Folder" 的点击选中逻辑
+            UseSelectedFolder();
+            Save();
+        }
+        // 新增按钮，用于删除目录
+        if (GUILayout.Button("Remove Folder"))
+        {
+            RemoveFolder();
+            Save();
         }
 
         // 刷新按钮
         if (GUILayout.Button("Refresh"))
         {
-            RefreshCounts();
+            RefreshCounts(selectedFolder);
         }
+
+        // 应用设置按钮
+        if (GUILayout.Button("Apply"))
+        {
+            ApplyMipmapsAndStreaming(selectedFolder);
+        }
+        // 关闭按钮
+        if (GUILayout.Button("Revert"))
+        {
+            DisableMipmapsAndStreaming(selectedFolder);
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
 
         // 显示统计信息
         GUILayout.Label("Total Textures: " + totalTextureCount);
         GUILayout.Label("Mip Streaming Enabled: " + mipStreamingEnabledCount);
 
-        // 关闭按钮
-        if (GUILayout.Button("Disable Mipmaps and Streaming"))
+        GUILayout.Space(20);
+        GUILayout.Label("Folder list", EditorStyles.boldLabel);
+        GUILayout.Label($"Folder selected: {selectedFolder}");
+        // 显示目录列表
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.ExpandHeight(true));
+        GUILayout.Space(10);
+        if (folderPaths != null)
         {
-            DisableMipmapsAndStreaming();
+            for (int i = 0; i < folderPaths.Count; i++)
+            {
+                GUILayout.BeginHorizontal();
+                bool isSelected = i == selectedFolderIndex;
+                isSelected = GUILayout.Toggle(isSelected, folderPaths[i], "Button");
+                if (isSelected)
+                {
+                    selectedFolderIndex = i;
+                    // folderPath = folderPaths[selectedFolderIndex];
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+        GUILayout.Space(10);
+        EditorGUILayout.EndScrollView();
+        GUILayout.Space(10);
+    }
+
+    // 在OnGUI之前调用，初始化目录列表
+    void OnEnable()
+    {
+        Load();
+    }
+
+    void RemoveFolder()
+    {
+        if (folderPaths.Count > 0)
+        {
+            // 移除选中的目录
+            folderPaths = folderPaths.Where((path, index) => index != selectedFolderIndex).ToList();
+
+            // 更新选中的目录索引
+            if (selectedFolderIndex >= folderPaths.Count)
+            {
+                selectedFolderIndex = folderPaths.Count - 1;
+            }
         }
     }
 
-    void ApplyMipmapsAndStreaming()
+    void RefreshCounts(string folderPath)
+    {
+        // 获取指定目录下的所有贴图文件
+        string[] texturePaths = Directory.GetFiles(folderPath, "*.png", SearchOption.AllDirectories);
+
+        totalTextureCount = texturePaths.Length;
+        mipStreamingEnabledCount = 0;
+
+        foreach (string texturePath in texturePaths)
+        {
+            // 导入贴图
+            TextureImporter textureImporter = AssetImporter.GetAtPath(texturePath) as TextureImporter;
+
+            // 记录启用了 Mip Streaming 的贴图数量
+            if (textureImporter != null && textureImporter.streamingMipmaps)
+            {
+                mipStreamingEnabledCount++;
+            }
+        }
+    }
+
+    void ApplyMipmapsAndStreaming(string folderPath)
     {
         // 获取指定目录下的所有贴图文件
         string[] texturePaths = Directory.GetFiles(folderPath, "*.png", SearchOption.AllDirectories);
@@ -91,28 +186,7 @@ public class MipmapAndStreamingWindow : EditorWindow
         Debug.Log("Mipmaps and Streaming settings applied to all textures in the specified folder.");
     }
 
-    void RefreshCounts()
-    {
-        // 获取指定目录下的所有贴图文件
-        string[] texturePaths = Directory.GetFiles(folderPath, "*.png", SearchOption.AllDirectories);
-
-        totalTextureCount = texturePaths.Length;
-        mipStreamingEnabledCount = 0;
-
-        foreach (string texturePath in texturePaths)
-        {
-            // 导入贴图
-            TextureImporter textureImporter = AssetImporter.GetAtPath(texturePath) as TextureImporter;
-
-            // 记录启用了 Mip Streaming 的贴图数量
-            if (textureImporter != null && textureImporter.streamingMipmaps)
-            {
-                mipStreamingEnabledCount++;
-            }
-        }
-    }
-
-    void DisableMipmapsAndStreaming()
+    void DisableMipmapsAndStreaming(string folderPath)
     {
         // 获取指定目录下的所有贴图文件
         string[] texturePaths = Directory.GetFiles(folderPath, "*.png", SearchOption.AllDirectories);
@@ -154,7 +228,7 @@ public class MipmapAndStreamingWindow : EditorWindow
             string selectedPath = AssetDatabase.GetAssetPath(selectedObjects[0]);
             if (Directory.Exists(selectedPath))
             {
-                folderPath = selectedPath;
+                folderPaths.Add(selectedPath);
             }
             else
             {
@@ -165,5 +239,17 @@ public class MipmapAndStreamingWindow : EditorWindow
         {
             Debug.LogWarning("Please select a folder in the Project window.");
         }
+    }
+
+    void Load()
+    {
+        if (File.Exists(filePath))
+            folderPaths = File.ReadAllLines(filePath).ToList();
+    }
+
+    void Save()
+    {
+        File.WriteAllLines(filePath, folderPaths);
+        AssetDatabase.Refresh();
     }
 }
